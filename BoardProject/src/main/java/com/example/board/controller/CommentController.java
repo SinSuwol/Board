b1bc4f3815f40c822e2100d0ac76854926514a46
SinSuwol <68939992+SinSuwol@users.noreply.github.com>
2025-10-01 17:10:33 +0900

    불필요한거 삭제

--- BoardProject/src/main/java/com/example/board/controller/CommentController.java
+++ BoardProject/src/main/java/com/example/board/controller/CommentController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("/comment")
